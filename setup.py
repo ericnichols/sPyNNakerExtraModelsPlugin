312783dd6528b5979f34ac816a0dafe57d441615
SpiNNaker <SpiNNaker@130.88.195.143>
2015-11-18 09:45:30 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ setup(
     url="https://github.com/SpiNNakerManchester/sPyNNakerExtraModelsPlugin",
     packages=['spynnaker_extra_pynn_models',
               'spynnaker_extra_pynn_models.model_binaries',
-              'spynnaker_extra_pynn_models.neural_models',
               'spynnaker_extra_pynn_models.neural_properties',
               'spynnaker_extra_pynn_models.neural_properties.synapse_dynamics',
-              'spynnaker_extra_pynn_models.neural_properties.synapse_dynamics.dependences'],
+              'spynnaker_extra_pynn_models.neural_properties.synapse_dynamics.dependences',
+              'spynnaker_extra_pynn_models.neuron.additional_inputs',
+              'spynnaker_extra_pynn_models.neuron.builds',
+              'spynnaker_extra_pynn_models.neuron.neural_models',
+              'spynnaker_extra_pynn_models.neuron.synapse_types',
+              'spynnaker_extra_pynn_models.neuron.threshold_types',],
     package_data={'spynnaker_extra_pynn_models.model_binaries': ['*.aplx']},
     install_requires=['SpyNNaker == 2015.005.01']
 )
